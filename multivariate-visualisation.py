0d82a08dfc81ec07a175c52c9f69a794c895aa7a
Thomas Bigger <thomas@performanceactive.com>
2019-05-15 16:50:59 +0100

    parallel_coordinates and scatter between 2 features

--- multivariate-visualisation.py
+++ multivariate-visualisation.py
@@ -39,7 +39,6 @@ wines = wines.sample(frac=1, random_state=42).reset_index(drop=True)
 
 # Correlation Matrix Heatmap (2D)
 f, ax = plt.subplots(figsize=(10, 6))
-
 corr = wines.corr()
 hm = sns.heatmap(corr, annot=True, ax=ax, cmap="coolwarm",fmt='.2f')
 f.subplots_adjust(top=0.93)
@@ -50,13 +49,42 @@ plt.savefig('wines-correlation-heatmap.pdf', format='pdf', dpi=1200)
 
 # Pair-wise Scatter Plots
 # observing potential relationships or patterns in two-dimensions
-cols = ['density', 'residual sugar', 'total sulfur dioxide', 'fixed acidity', 'quality']
-pp = sns.pairplot(wines[cols], height=1.8, aspect=1.8,
+cols = ['density', 'residual sugar', 'total sulfur dioxide', 'fixed acidity']
+subset_df = wines[cols]
+
+pp = sns.pairplot(subset_df, height=1.8, aspect=1.8,
                   plot_kws=dict(edgecolor="k", linewidth=0.5),
                   diag_kind="kde", diag_kws=dict(shade=True))
-
 fig = pp.fig 
 fig.subplots_adjust(top=0.93, wspace=0.3)
 t = fig.suptitle('Wine Attributes Pairwise Plots', fontsize=14)
 #plt.savefig('wines-pairwise-scatter.pdf', format='pdf', dpi=1200)
+
+
+plt.clf()
+
+
+
+# Scaling attribute values to avoid few outiers
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+
+scaled_df = ss.fit_transform(subset_df)
+scaled_df = pd.DataFrame(scaled_df, columns=cols)
+final_df = pd.concat([scaled_df, wines['wine_type']], axis=1)
+final_df.head()
+
+# plot parallel coordinates
+from pandas.plotting import parallel_coordinates
+pc = parallel_coordinates(final_df, 'wine_type', color=('#FFE888', '#FF9999'))
+plt.savefig('wines-parallel-coords.pdf', format='pdf', dpi=1200)
+
+
+# Scatter Plot
+plt.scatter(wines['sulphates'], wines['alcohol'],
+            alpha=0.4, edgecolors='w')
+plt.xlabel('Sulphates')
+plt.ylabel('Alcohol')
+plt.title('Wine Sulphates - Alcohol Content',y=1.05)
+plt.savefig('sulphate-alcohol-scatter.pdf', format='pdf', dpi=1200)
 
