8bceb73e21bb950aa8a1cd02c9c1efc394128420
Thomas Bigger <thomas@performanceactive.com>
2019-05-15 12:45:15 +0100

    Added standard deviation lines and some tweaks

--- wine-tastin-vis.py
+++ wine-tastin-vis.py
@@ -36,7 +36,6 @@ white_wine['quality_label'] = pd.Categorical(white_wine['quality_label'],
                                              categories=['low', 'medium', 'high'])
 
 
-
 # merge red and white wine datasets
 wines = pd.concat([red_wine, white_wine])
 
@@ -44,12 +43,13 @@ wines = pd.concat([red_wine, white_wine])
 wines = wines.sample(frac=1, random_state=42).reset_index(drop=True)
 
 
-
+# getting overall stats for specific features
 subset_attributes = ['residual sugar', 'total sulfur dioxide', 'sulphates',
                      'alcohol', 'volatile acidity', 'quality']
 rs = round(red_wine[subset_attributes].describe(),2)
 ws = round(white_wine[subset_attributes].describe(),2)
-
+#overall stats
+wines_desc=round(wines[subset_attributes].describe(),2)
 # concatinate the descriptions but can easier open the two windows
 stats=pd.concat([rs, ws], axis=1, keys=['Red Wine Statistics', 'White Wine Statistics'])
 
@@ -60,15 +60,30 @@ wines.hist(bins=15, color='steelblue', edgecolor='black', linewidth=0.8,
 plt.tight_layout()
 #plt.savefig('wines-hist.pdf', format='pdf', dpi=1200)
 
-# Histogram for a specific
+
+# Histogram for a specific feature
+feature_name='sulphates'
+feature=wines[feature_name]
+mean=feature.mean()
+std=feature.std()
+
 fig = plt.figure(figsize = (6,4))
-title = fig.suptitle("Sulphates Content in Wine", fontsize=14)
+title = fig.suptitle(feature_name + " Content in Wine", fontsize=14)
 fig.subplots_adjust(top=0.85, wspace=0.3)
 
 ax = fig.add_subplot(1, 1, 1)
-ax.set_xlabel("Sulphates")
+ax.set_xlabel(feature_name)
 ax.set_ylabel("Frequency")
+ax.text(x=feature.max(), y=800, s= r'$\mu$ mean='+str(round(mean,2)), fontsize=12,  horizontalalignment='right',)
+ax.axvline(x=mean)
+ax.axvline(x=(mean - std), color='r')
+ax.axvline(x=(mean + std), color='g')
 
-freq, bins, patches = ax.hist(wines['sulphates'], color='steelblue', bins=15,
+freq, bins, patches = ax.hist(feature, color='steelblue', bins=25,
                                     edgecolor='black', linewidth=1)
-#plt.savefig('sulphate-freq.pdf', format='pdf', dpi=1200)
+#plt.savefig(feature_name + '-freq.pdf', format='pdf', dpi=1200)
+
+
+
+
+
