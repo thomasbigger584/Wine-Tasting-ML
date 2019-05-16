248a9786f43d46f0b2914f52fbf204ceea1279d7
Thomas Bigger <thomas@performanceactive.com>
2019-05-16 12:13:34 +0100

    more plots in on 3d

--- 3d-visualisation.py
+++ 3d-visualisation.py
@@ -36,3 +36,68 @@ wines = pd.concat([red_wine, white_wine])
 # re-shuffle records just to randomize data points
 wines = wines.sample(frac=1, random_state=42).reset_index(drop=True)
 
+
+
+#Considering three attributes or dimensions in the data, we can visualize them by 
+#considering a pair-wise scatter plot and introducing the notion of color or hue 
+#to separate out values in a categorical dimension.
+# Scatter Plot with Hue for visualizing data in 3-D
+cols = ['density', 'residual sugar', 'total sulfur dioxide', 'fixed acidity', 'wine_type']
+pp = sns.pairplot(wines[cols], hue='wine_type', height=1.8, aspect=1.8, 
+                  palette={"red": "#FF9999", "white": "#FFE888"},
+                  plot_kws=dict(edgecolor="black", linewidth=0.5))
+fig = pp.fig 
+fig.subplots_adjust(top=0.93, wspace=0.3)
+t = fig.suptitle('Wine Attributes Pairwise Plots', fontsize=14)
+#plt.savefig('wines-pairwise-scatter-colored-3d.pdf', format='pdf', dpi=1200)
+
+
+
+#visualizing three continuous, numeric attributes
+# Visualizing 3-D numeric data with Scatter Plots
+# length, breadth and depth
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure(figsize=(8, 6))
+ax = Axes3D(fig)
+
+xs = wines['residual sugar']
+ys = wines['fixed acidity']
+zs = wines['alcohol']
+ax.scatter(xs, ys, zs, s=50, alpha=0.6, edgecolors='w')
+
+ax.set_xlabel('Residual Sugar')
+ax.set_ylabel('Fixed Acidity')
+ax.set_zlabel('Alcohol')
+#plt.savefig('sugar-acidity-alcohol-scatter-3d.pdf', format='pdf', dpi=1200)
+
+
+
+#We can also still leverage the regular 2-D axes and introduce the notion of 
+#size as the third dimension (essentially a bubble chart) where the size of 
+#the dots indicate the quantity of the third dimension.
+# Visualizing 3-D numeric data with a bubble chart
+# length, breadth and size
+plt.scatter(wines['fixed acidity'], wines['alcohol'], s=wines['residual sugar']*25, 
+            alpha=0.4, edgecolors='w')
+
+plt.xlabel('Fixed Acidity')
+plt.ylabel('Alcohol')
+plt.title('Wine Alcohol Content - Fixed Acidity - Residual Sugar',y=1.05)
+#plt.savefig('sugar-acidity-alcohol-bubble.pdf', format='pdf', dpi=1200)
+
+
+#For visualizing three discrete, categorical attributes, while we can use the conventional 
+#bar plots, we can leverage the notion of hue as well as facets or subplots to 
+#support the additional third dimension.
+# Visualizing 3-D categorical data using bar plots
+# leveraging the concepts of hue and facets
+fc = sns.catplot(x="quality", hue="wine_type", col="quality_label", 
+                    data=wines, kind="count",
+                    palette={"red": "#FF9999", "white": "#FFE888"})
+#plt.savefig('categry-plot-3d.pdf', format='pdf', dpi=1200)
+
+
+
+
+
+
