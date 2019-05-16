569a2cc46193137dd4828e89e4a1d9eceba6bb71
Thomas Bigger <thomas@performanceactive.com>
2019-05-16 09:18:17 +0100

    Added categorical bar plots of different styles but convery similar meaning. prefer sns countplot to visualise on the one graph. also added joint plot pdf printout

--- multivariate-visualisation.py
+++ multivariate-visualisation.py
@@ -60,10 +60,7 @@ fig.subplots_adjust(top=0.93, wspace=0.3)
 t = fig.suptitle('Wine Attributes Pairwise Plots', fontsize=14)
 #plt.savefig('wines-pairwise-scatter.pdf', format='pdf', dpi=1200)
 
-
 plt.clf()
-
-
 
 # Scaling attribute values to avoid few outiers
 from sklearn.preprocessing import StandardScaler
@@ -77,7 +74,7 @@ final_df.head()
 # plot parallel coordinates
 from pandas.plotting import parallel_coordinates
 pc = parallel_coordinates(final_df, 'wine_type', color=('#FFE888', '#FF9999'))
-plt.savefig('wines-parallel-coords.pdf', format='pdf', dpi=1200)
+#plt.savefig('wines-parallel-coords.pdf', format='pdf', dpi=1200)
 
 
 # Scatter Plot
@@ -86,5 +83,49 @@ plt.scatter(wines['sulphates'], wines['alcohol'],
 plt.xlabel('Sulphates')
 plt.ylabel('Alcohol')
 plt.title('Wine Sulphates - Alcohol Content',y=1.05)
-plt.savefig('sulphate-alcohol-scatter.pdf', format='pdf', dpi=1200)
+#plt.savefig('sulphate-alcohol-scatter.pdf', format='pdf', dpi=1200)
 
+# Joint Plot
+# you can check out correlations, relationships as well as individual distributions in the joint plot
+jp = sns.jointplot(x='sulphates', y='alcohol', data=wines,
+                   kind='reg', space=0, height=5, ratio=4)
+#plt.savefig('sulphate-alcohol-joint.pdf', format='pdf', dpi=1200)
+
+#visualizing two discrete, categorical attributes
+# Using subplots or facets along with Bar Plots
+fig = plt.figure(figsize = (10, 4))
+title = fig.suptitle("Wine Type - Quality", fontsize=14)
+fig.subplots_adjust(top=0.85, wspace=0.3)
+# red wine - wine quality
+ax1 = fig.add_subplot(1,2, 1)
+ax1.set_title("Red Wine")
+ax1.set_xlabel("Quality")
+ax1.set_ylabel("Frequency")
+rw_q = red_wine['quality'].value_counts()
+rw_q = (list(rw_q.index), list(rw_q.values))
+# set this on both to have the same range on the graph
+ax1.set_ylim([0, 2500])
+ax1.tick_params(axis='both', which='major', labelsize=8.5)
+bar1 = ax1.bar(rw_q[0], rw_q[1], color='red',
+               edgecolor='black', linewidth=1)
+
+
+# white wine - wine quality
+ax2 = fig.add_subplot(1,2, 2)
+ax2.set_title("White Wine")
+ax2.set_xlabel("Quality")
+ax2.set_ylabel("Frequency")
+ww_q = white_wine['quality'].value_counts()
+ww_q = (list(ww_q.index), list(ww_q.values))
+ax2.set_ylim([0, 2500])
+ax2.tick_params(axis='both', which='major', labelsize=8.5)
+bar2 = ax2.bar(ww_q[0], ww_q[1], color='white',
+               edgecolor='black', linewidth=1)
+#plt.savefig('wine-type-quality-freq.pdf', format='pdf', dpi=1200)
+
+
+#Another good way is to use stacked bars or multiple bars for the different attributes in a single plot.
+# Multi-bar Plot
+cp = sns.countplot(x="quality", hue="wine_type", data=wines,
+                   palette={"red": "#FF9999", "white": "#FFE888"})
+plt.savefig('wine-type-quality-count.pdf', format='pdf', dpi=1200)
